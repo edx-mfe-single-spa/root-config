c1e08f0189052c95256de5ce7af9346f3d1fea3d
Adam Stankiewicz <agstanki@gmail.com>
2020-07-16 07:17:40 -0400

    add some mfes

--- src/edx-root-config.js
+++ src/edx-root-config.js
@@ -3,7 +3,25 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
   name: "@edx/navbar",
   app: () => System.import("@edx/navbar"),
-  activeWhen: ["/"],
+  activeWhen: () => true,
+});
+
+registerApplication({
+  name: "@edx/enterprise-learner-portal",
+  app: () => System.import("@edx/enterprise-learner-portal"),
+  activeWhen: ["/enterprise"],
+});
+
+registerApplication({
+  name: "@edx/course-learning",
+  app: () => System.import("@edx/course-learning"),
+  activeWhen: ["/course"],
+});
+
+registerApplication({
+  name: "@edx/user-profile",
+  app: () => System.import("@edx/user-profile"),
+  activeWhen: ["/profile"],
 });
 
 start({
